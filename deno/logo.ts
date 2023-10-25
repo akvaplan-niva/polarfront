b7c4227adc322f724af3e4faa1baf9ebe35e3685
cnrdh <cnrdh@users.noreply.github.com>
2023-10-25 14:31:47 +0200

    fix oa tracker

--- deno/logo.ts
+++ deno/logo.ts
@@ -1,4 +1,4 @@
-import { encodeBase64 } from "https​://deno.land/std​@0.204.0/encoding/base64.ts";
+import { encodeBase64 } from "https://deno.land/std@0.204.0/encoding/base64.ts";
 export const logo400WBase64 = encodeBase64(
   await Deno.readFile("logo/polarfront_400w.png"),
 );
